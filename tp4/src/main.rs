a62af51c070e7382f98d64b8eeba7b22602eacf2
JulienGuillaud <julien.guillaud00@gmail.com>
2022-05-04 10:45:44 +0200

    tp4 fonctionnel sans optimisation

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -1,60 +1,106 @@
-use crate::Instruction::*;
-use std::io;
+use std::io::Read;
 
 fn main() {
-    let code = vec![Lis, Plus, Affiche, Lis, Plus, Affiche, Lis, Plus, Lis, Plus, Affiche];
-    let mut memoire = vec![0];
+    let mut memoire = vec![10, 15];
+    // let instructions = vec![Boucle(vec![Moins, Droite, Plus, Gauche])];
+    let instructions = parse(&"+-<>><><>.,".to_string()).unwrap_or(vec![]);
 
     println!("MEMOIRE DEPART : {:?}", memoire);
 
-    interpreteur(&mut memoire, &code);
+    interpreteur(&mut memoire, &instructions, 0);
     println!("RESULTAT FINAL : {:?}", memoire)
 
 }
 
 
-fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>){
-    let mut mem_index = 0;
+fn parse(s: &String) -> Result<Vec<Instruction>, String>{
+    // Boucle each char of string
+    let mut instructions = vec![];
+    for c in s.chars() {
+        match c {
+            '+' => instructions.push(Instruction::Plus),
+            '-' => instructions.push(Instruction::Moins),
+            '<' => instructions.push(Instruction::Gauche),
+            '>' => instructions.push(Instruction::Droite),
+            ',' => instructions.push(Instruction::Lis),
+            '.' => instructions.push(Instruction::Affiche),
+            _ => return Err(format!("Caractère non reconnu : {}", c)),
+        }
+    }
+    Ok(instructions)
+}
+
+fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>, index: usize) {
+
+    let mut mem_index = index;
     for instruction_index in 00..instructions.len() {
         let instruction = instructions[instruction_index].clone();
+        println!("  mem avant {:?}", memoire);
         match instruction {
-            Plus => memoire[mem_index] += 1,
-            Moins => memoire[mem_index] -= 1,
-            Droite => move_side(memoire, &mut mem_index, instruction),
-            Gauche => move_side(memoire, &mut mem_index, instruction),
-            Affiche => println!("mem[{}] : {:?}", mem_index, memoire[mem_index]),
-            Lis => {
-                let my_int = stdin.read(guess);
-                memoire[mem_index] = my_int;
+            Instruction::Plus => {
+                println!("+");
+                memoire[mem_index] += 1
+            }
+            Instruction::Moins => {
+                println!("-");
+                memoire[mem_index] -= 1
+            }
+            Instruction::Droite => {
+                println!("D");
+                mem_index = move_side(memoire, mem_index, instruction);
+
+            }
+            Instruction::Gauche => {
+                println!("G");
+                mem_index = move_side(memoire, mem_index, instruction);
+            }
+            Instruction::Affiche => println!("mem[{}] : {:?}", mem_index, memoire[mem_index]),
+            Instruction::Lis => {
+                println!("Entrez un nombre : ");
+                // input int from console
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input);
+                println!("input : {}",   input.trim().parse::<i32>().unwrap());
+                memoire[mem_index] = input.trim().parse::<i32>().unwrap();
+            }
+            Instruction::Boucle(les_instructions) => {
+                while memoire[mem_index] != 0
+                {
+                    println!("lesInstructions : {:?}", &les_instructions);
+                    interpreteur(memoire, &les_instructions, mem_index);
+                }
             }
         }
     }
 }
 
-fn move_side(memoire: &mut Vec<i32>, mem_index: &mut usize, side: Instruction) {
+fn move_side(memoire: &mut Vec<i32>, mem_index: usize, side: Instruction) -> usize{
+    let mut newIndex = mem_index;
     match side {
-        Droite => {
-            *mem_index += 1;
-            if *mem_index >= memoire.len(){
+        Instruction::Droite => {
+            newIndex += 1;
+            if newIndex >= memoire.len(){
                 memoire.push(0)
             }
         },
-        Gauche => {
-            if *mem_index != 0 {
-                *mem_index -= 1
+        Instruction::Gauche => {
+            if newIndex != 0 {
+                newIndex -= 1
             }else{
                 println!("Dépassement de la limite de la memoire (coté gauche, ne pas se déplacer)")
             }
         }
-        Plus => {}
-        Moins => {}
-        Affiche => {}
-        Lis => {}
+        Instruction::Plus => {}
+        Instruction::Moins => {}
+        Instruction::Affiche => {}
+        Instruction::Lis => {}
+        Instruction::Boucle(les_instructions) => {}
     }
+    newIndex
 }
 
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Instruction {
     Plus,
     Moins,
@@ -62,4 +108,5 @@ enum Instruction {
     Gauche,
     Affiche,
     Lis,
+    Boucle(Vec<Instruction>)
 }
